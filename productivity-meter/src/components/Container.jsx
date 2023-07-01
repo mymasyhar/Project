ca246b27943985f663fd486341c985ec13bbfaaf
mymasyhar <mymasyhar@gmail.com>
2023-07-01 21:13:05 +0700

    initializing

---
+++ productivity-meter/src/components/Container.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class Container extends Component {
+  render(){
+    return(
+      <section>
+        <h1>This will be the container for your activity list</h1>
+      </section>
+    )
+  }
+}
