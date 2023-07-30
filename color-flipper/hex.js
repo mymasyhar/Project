c99117a9fff0c1d1f0b30dfcd95445273e04c530
mymasyhar <mymasyhar@gmail.com>
2023-07-31 06:26:26 +0700

    separating characters with function

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -1,15 +1,19 @@
 const hex = ["1","2","3","4","5","6","A","B","C","D","E","F"];
 
+function getNumber(){
+  return Math.floor(Math.random() * hex.length)
+}
+
 function getHexColor(){
   let hexColor = '#';
   for(let i=0; i<6; i++){
-    hexColor += hex[Math.floor(Math.random() * hex.length)];
+    const number = getNumber();
+    hexColor += hex[number];
   }
   return hexColor;
 }
 
 const span = document.querySelector('.color');
-
 const btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
   const color = getHexColor();
